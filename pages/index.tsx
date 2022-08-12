ad62a1dd27e6b88b9d8e1a9f5943b9a0cec27138
HYEOK9 <leejaehyuck9@gmail.com>
2022-08-12 15:19:32 +0900

    Initial commit(create next-app,tailwind+styled 세팅

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import tw from "tailwind-styled-components";
+
+const Home: NextPage = () => {
+    return (
+        <Div>
+            <h1>:D</h1>
+        </Div>
+    );
+};
+
+export default Home;
+
+const Div = tw.div`
+w-screen
+h-screen
+bg-black
+&>h1{
+ text-white
+}
+`;

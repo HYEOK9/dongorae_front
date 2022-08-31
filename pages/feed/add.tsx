3eced4493898cb63ba5cb5e6349a5ba38f9d1e4e
wjdwl002 <wjdwl001@khu.ac.kr>
2022-08-31 18:02:40 +0900

    feature:[피드등록] 피드 등록페이지 추가

---
+++ pages/feed/add.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState, useRef } from "react";
+import tw from "tailwind-styled-components";
+
+const AddFeed = () => {
+
+    return (<>
+    <MainContainer>
+    피드 등록 페이지입니다.
+    </MainContainer>
+    </>)
+}
+
+const MainContainer = tw.div`
+w-full h-full
+flex justify-center
+`
+
+export default AddFeed;

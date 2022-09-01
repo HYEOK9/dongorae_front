52b7c97f60efe0beb1b907c944f1987dd0416200
wjdwl002 <wjdwl001@khu.ac.kr>
2022-09-01 12:49:30 +0900

    feature:[피드등록] 피드 등록페이지에 slate 라이브러리 추가

---
+++ components/common/textEditor.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState, useRef } from "react";
+
+import { createEditor , BaseEditor, Descendant} from 'slate'
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue = [
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]
+
+const TextEditor = () => {
+    const [editor] = useState(() => withReact(createEditor()))
+
+    return (
+        <Slate editor={editor} value={initialValue}>
+            <Editable/>
+        </Slate>
+    )
+}
+
+export default TextEditor;

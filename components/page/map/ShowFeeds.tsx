4ab3a3686ab68b4a643d0f05d0500a256ef65dfc
HYEOK9 <leejaehyuck9@gmail.com>
2022-09-13 16:54:10 +0900

    feat:[map] 컴포넌트 분리

---
+++ components/page/map/ShowFeeds.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+//components
+import Feed from "../../../components/page/home/Feed";
+//types
+import { IFeedThumbnail } from "../../../types/feed";
+//style
+import tw from "tailwind-styled-components";
+import LoadingSVG from "/public/loading.svg";
+import { useTheme } from "../../../components/context/Theme";
+
+interface propType {
+    curFeeds: any[];
+    filterOption: any;
+    user: any;
+    searchLoading: boolean;
+    map: any;
+}
+const ShowFeeds = ({
+    curFeeds,
+    filterOption,
+    user,
+    searchLoading,
+    map,
+}: propType) => {
+    //검색결과 존재여부
+    const [isEmpty, setIsEmpty] = useState<boolean>(false);
+    const { themeColorset } = useTheme();
+
+    useEffect(() => {
+        if (map == null) return;
+        if (filterOption === "전체" && curFeeds.length === 0) {
+            setIsEmpty(true);
+            return;
+        }
+
+        if (
+            filterOption === "맞춤게시물" &&
+            user &&
+            curFeeds.filter(
+                (feed) =>
+                    feed.sensedata.auditory <= user.userSense.auditory &&
+                    feed.sensedata.visual <= user.userSense.visual &&
+                    feed.sensedata.vestibular <= user.userSense.vestibular &&
+                    feed.sensedata.tacticle <= user.userSense.tactile &&
+                    feed.sensedata.proprioceptive <=
+                        user.userSense.proprioceptive &&
+                    feed.sensedata.oral <= user.userSense.oral
+            ).length === 0
+        ) {
+            setIsEmpty(true);
+            return;
+        }
+        setIsEmpty(false);
+    }, [curFeeds, filterOption]);
+
+    return (
+        <HomeContainer>
+            <FeedContainer>
+                {searchLoading && (
+                    <LoadingSVGwrap>
+                        <LoadingSVG
+                            width={50}
+                            height={50}
+                            fill={themeColorset.textColor}
+                        />
+                    </LoadingSVGwrap>
+                )}
+                {isEmpty ? (
+                    <div style={{ marginTop: "50px" }}>게시물이 없습니다.</div>
+                ) : filterOption === "전체" ? (
+                    curFeeds.map((feed: any) => (
+                        <Feed key={feed.feedId} data={feed} />
+                    ))
+                ) : filterOption === "맞춤게시물" && user ? (
+                    curFeeds.filter(
+                        (feed) =>
+                            feed.sensedata.auditory <=
+                                user.userSense.auditory &&
+                            feed.sensedata.visual <= user.userSense.visual &&
+                            feed.sensedata.vestibular <=
+                                user.userSense.vestibular &&
+                            feed.sensedata.tacticle <= user.userSense.tactile &&
+                            feed.sensedata.proprioceptive <=
+                                user.userSense.proprioceptive &&
+                            feed.sensedata.oral <= user.userSense.oral
+                    )
+                ) : null}
+            </FeedContainer>
+        </HomeContainer>
+    );
+};
+
+export default ShowFeeds;
+
+const HomeContainer = tw.section`
+flex justify-center w-screen py-[10px]
+`;
+
+const FeedContainer = tw.div`
+w-[90vw] max-w-[1800px] flex flex-wrap justify-center
+`;
+
+const LoadingSVGwrap = tw.div`
+absolute top-[55%] left-[49%] animate-spin z-50
+`;
--- pages/searchmap/index.tsx
+++ pages/searchmap/index.tsx
@@ -7,6 +7,7 @@ import { setFilterOption, setSenseData } from "../../store/filterSlice";
 //components
 import Feed from "../../components/page/home/Feed";
 import SearchFiler from "../../components/common/filtering/searchFilter";
+import ShowFeeds from "../../components/page/map/ShowFeeds";
 //hooks
 import useCurLocation from "../../util/hooks/useCurLocation";
 import useInitMap from "../../util/hooks/map/useInitMap";
@@ -18,14 +19,14 @@ import displayMarker from "../../util/hooks/map/displayMarker";
 import removeMarker from "../../util/hooks/map/removeMarker";
 //types
 import { MarkerType } from "../../types/map";
+import { IFeedThumbnail } from "../../types/feed";
 //style
 import tw from "tailwind-styled-components";
 import LoadingSVG from "/public/loading.svg";
 import { useTheme } from "../../components/context/Theme";
 
 const searchmap = () => {
-    console.log("re-render");
-    const userId = useStayLogin();
+    const { user } = useStayLogin();
     // 유저 위치 fetch
     const { location, locError } = useCurLocation();
     // 맵 초기화 , {지도, 지도를 띄울 dom의 ref}
@@ -36,11 +37,9 @@ const searchmap = () => {
     const keyword = useSelector(
         (state: RootState) => state.searchState.keyword
     );
-    // 키워드로 검색 , [결과, 에러 여부]
-    //const { result, searchError } = useKakaoMapSearch(keyword);
-    // 검색 후 보여지는 피드들 List
+    // 현재 보여지는 피드 Arr
     const [curFeeds, setCurFeeds] = useState<any[]>([]);
-    // 검색 후 보여지는 피드들의 마커 List (마커 지우려면 필요)
+    // 피드들 고래마커 Arr
     const [curPlacesMarkers, setCurPlacesMarkers] = useState<MarkerType[]>([
         {
             marker: null,
@@ -52,19 +51,10 @@ const searchmap = () => {
     const filterOption = useSelector(
         (state: RootState) => state.filterState.option
     );
-    ///검색 필터의 senseData 값
-    const senseData = useSelector(
-        (state: RootState) => state.filterState.senseData
-    );
-    //검색결과 존재여부
-    const [isEmpty, setIsEmpty] = useState<boolean>(false);
+
     const { themeColorset } = useTheme();
     const dispatch = useDispatch();
 
-    // const isInMap = (lat: number, long: number) => {
-    //     return curMapSize.contain(new kakao.maps.LatLng(lat, long));
-    // };
-    console.log(searchLoading);
     const searchHere = async () => {
         dispatch(setSenseData(null));
         setSearchLoading(true);
@@ -81,6 +71,7 @@ const searchmap = () => {
 
     useEffect(() => {
         if (map === null || keyword.trim() == "") return;
+
         (async () => {
             setSearchLoading(true);
             const res = await keywordSearch(keyword);
@@ -94,21 +85,6 @@ const searchmap = () => {
 
     useEffect(() => {
         if (map === null) return;
-        switch (filterOption) {
-            case "전체":
-                dispatch(setSenseData(null));
-                break;
-            case "맞춤게시물":
-                dispatch(setSenseData(null));
-                break;
-            case "필터":
-                break;
-        }
-    }, [filterOption]);
-
-    useEffect(() => {
-        // 처음 렌더할 때 결과 없음 문구 안보여주려고 map&& 넣음
-        map && curFeeds.length === 0 ? setIsEmpty(true) : setIsEmpty(false);
         setSearchLoading(false);
         const places = Array.from(
             new Set(
@@ -121,10 +97,14 @@ const searchmap = () => {
                 )
             )
         ).map((feed: any) => JSON.parse(feed));
+
         removeMarker(curPlacesMarkers, setCurPlacesMarkers);
+
         places.forEach((place) => {
             displayMarker(place, map, setCurPlacesMarkers);
         });
+
+        //키워드로 검색한 경우
         if (curFeeds.length == 1 && keyword != "") {
             const bounds = new kakao.maps.LatLngBounds();
             bounds.extend(
@@ -141,7 +121,7 @@ const searchmap = () => {
         <>
             <Container>
                 <MapContainer>
-                    {!location && !locError && (
+                    {!location && locError && (
                         <>
                             <Loading>위치 정보를 가져오는 중입니다</Loading>
                             <LoadingSVGwrap>
@@ -159,24 +139,13 @@ const searchmap = () => {
                 </MapContainer>
                 {/*filtering*/}
                 <SearchFiler />
-                <HomeContainer>
-                    <FeedContainer>
-                        {searchLoading && (
-                            <LoadingSVGwrap>
-                                <LoadingSVG width={50} height={50} />
-                            </LoadingSVGwrap>
-                        )}
-                        {isEmpty ? (
-                            <div style={{ marginTop: "50px" }}>
-                                게시물이 없습니다.
-                            </div>
-                        ) : (
-                            curFeeds.map((feed: any) => (
-                                <Feed key={feed.feedId} data={feed} />
-                            ))
-                        )}
-                    </FeedContainer>
-                </HomeContainer>
+                <ShowFeeds
+                    curFeeds={curFeeds}
+                    filterOption={filterOption}
+                    user={user}
+                    searchLoading={searchLoading}
+                    map={map}
+                />
             </Container>
         </>
     );
@@ -206,12 +175,4 @@ relative w-full h-[45vh]
 
 const SearchHereBtn = tw.div`
 absolute top-[80%] px-4 py-3 font-semibold sm:text-xs sm:px-2 sm:py-1 rounded-2xl z-30 cursor-pointer shadow-2xl hover:scale-[0.98]
-`;
-
-const HomeContainer = tw.section`
-flex justify-center w-screen py-[10px]
-`;
-
-const FeedContainer = tw.div`
-w-[90vw] max-w-[1800px] flex flex-wrap justify-center
 `;

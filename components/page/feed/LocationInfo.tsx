50f05f2f69f2c6777915700fd112c6e170aba6c8
wjdwl002 <wjdwl001@khu.ac.kr>
2022-09-13 14:14:00 +0900

    feat:[피드상세] location 클릭시 search map으로 연결

--- components/page/feed/LocationInfo.tsx
+++ components/page/feed/LocationInfo.tsx
@@ -4,6 +4,7 @@ import { useTheme } from '../../context/Theme';
 import styled from 'styled-components';
 import CallMade from '@mui/icons-material/CallMade';
 import { ILocation } from '../../../types/feed';
+import { useRouter } from 'next/router';
 
 interface PropType{
     data: ILocation
@@ -12,10 +13,11 @@ interface PropType{
 const LocationInfo = (props: PropType) => {
     const { themeColorset } = useTheme();
     const { data } = props;
+    const router = useRouter();
 
     return (<>
     <LocationContainer>
-        <LocationHolder>
+        <LocationHolder onClick={()=> {router.push('/searchmap')}}>
             <CallMade/> {data?.placeName} 
         </LocationHolder>
         <AddressHolder>
@@ -32,6 +34,7 @@ flex flex-col gap-[6px]
 
 const LocationHolder = tw.div`
 flex items-center gap-[4px]
+hover:underline underline-offset-2
 `
 
 const AddressHolder = tw.div`
